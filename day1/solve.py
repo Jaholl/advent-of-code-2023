842598da22188c5ea71a1ae198db58a3e5f8668f
Joel Bångdal <j.bangdal@gmail.com>
2023-12-01 20:47:07 +0100

    Day 1 completed

---
+++ day1/solve.py
@@ -0,0 +1,34 @@
+import re
+
+file = open("input", "r")
+lines = file.readlines()
+
+def convert_number(numb):
+    match numb:
+            case 'one'|'eno':
+                return '1'
+            case 'two'|'owt':
+                return '2'
+            case 'three'|'eerht':
+                return '3'
+            case 'four'|'ruof':
+                return '4'
+            case 'five'|'evif':
+                return '5'
+            case 'six'|'xis':
+                return '6'
+            case 'seven'|'neves':
+                return '7'
+            case 'eight'|'thgie':
+                return '8'
+            case 'nine'|'enin':
+                return '9'
+    return numb
+        
+found_numbers = []
+for line in lines:
+    match1 = convert_number(re.search("\d|one|two|three|four|five|six|seven|eight|nine", line).group())
+    match2 = convert_number(re.search("\d|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin", line[::-1]).group())
+    
+    found_numbers.append(int(str(match1)+str(match2)))
+print(sum(found_numbers))
