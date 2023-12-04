3dd9e8afcf23ba5784310c73e7f8020fe43d8f01
Joel Bångdal <j.bangdal@gmail.com>
2023-12-04 09:09:25 +0100

    computer about to die

--- day4/solvep1.py
+++ day4/solvep1.py
@@ -2,3 +2,13 @@ import re
 
 file = open("day4/input.txt", "r")
 lines = file.read().split("\n")
+
+total = 0
+for line in lines:
+    cardworth = 0
+    input = line.split(":")[1]
+    winnernumbers = input.split("|")[0]
+    ournumbers = input.split("|")[1]
+    winnernumbers = re.findall("\d+", winnernumbers)
+
+    print(winnernumbers)
