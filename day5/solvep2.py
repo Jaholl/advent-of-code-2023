f1476b2b0df7d2af4807f5f2503bcce2a7fce96e
Joel Bångdal <j.bangdal@gmail.com>
2023-12-05 19:02:27 +0100

    Day 5 Completed

--- day5/solvep2.py
+++ day5/solvep2.py
@@ -12,11 +12,11 @@ def source_destination_range_map(input):
   fullrange = -1
   for r in range(len(input)):
     if r % 3 == 0:
-      destination = int(input[r])
+      destination = float(input[r])
     if r % 3 == 1:
-      source = int(input[r])
+      source = float(input[r])
     if r % 3 == 2:
-      fullrange = int(input[r])
+      fullrange = float(input[r])
     if destination != -1 and source != -1 and fullrange != -1:
       returnmap[str(source) + ',' + str(fullrange)] = str(destination)
       destination = -1
@@ -24,12 +24,24 @@ def source_destination_range_map(input):
       fullrange = -1
   return returnmap
 
-def find_if_in_interval(item, list):
+def find_if_in_interval(item, list, nextList, jump):
   for key in list.keys():
     source, fullrange = key.split(',')
-    difference = int(item) - int(source)
-    if int(item) >= int(source) and int(item) <= int(source) + int(fullrange):
-      return (int(list[key]) + difference)
+    if float(item) >= float(source) and float(item) < float(source) + float(fullrange):
+      difference = float(item) - float(source)
+      print("Difference:", difference)
+      if 0 < float(fullrange) + float(source) - float(item):
+        jump.append(float(fullrange) + float(source) - float(item))
+      else:
+        jump.append(1)
+      return (float(list[key]) + difference)
+  
+  smallestjump = 99999999999
+  for key in nextList.keys():
+    nextsource, _ = key.split(',')
+    if 0 < float(nextsource) - float(item) < smallestjump:
+      smallestjump = float(nextsource) - float(item)
+  jump.append(smallestjump)
   return (item)
 
 categoryInput = []
@@ -41,9 +53,9 @@ seed = -1
 fullrange = -1
 for r in range(len(categoryInput[1])):
   if r % 2 == 0:
-    seed = int(categoryInput[1][r])
+    seed = float(categoryInput[1][r])
   if r % 2 == 1:
-    fullrange = int(categoryInput[1][r])
+    fullrange = float(categoryInput[1][r])
   if seed != -1 and fullrange != -1:
     seeds_to_plant.append((seed, fullrange))
     seed = -1
@@ -62,21 +74,25 @@ for key, value in seeds_to_plant:
   index = key
   print("New seed: ", key, key+value)
   while index <= key+value:
-    if index % 100000 == 0:
-      print(index, key+value)
-    soil = find_if_in_interval(index, seed_to_soil)
-    fertilizer = find_if_in_interval(soil, soil_to_fertilizer)
-    water = find_if_in_interval(fertilizer, fertilizer_to_water)
-    light = find_if_in_interval(water, water_to_light)
-    temp = find_if_in_interval(light, light_to_temperature)
-    hum = find_if_in_interval(temp, temperature_to_humidity)
-    loc = find_if_in_interval(hum, humidity_to_location)
+    print(index, key+value)
+    jump = []
+    soil = find_if_in_interval(index, seed_to_soil, soil_to_fertilizer, jump)
+    fertilizer = find_if_in_interval(soil, soil_to_fertilizer, fertilizer_to_water, jump)
+    water = find_if_in_interval(fertilizer, fertilizer_to_water, water_to_light, jump)
+    light = find_if_in_interval(water, water_to_light, light_to_temperature, jump)
+    temp = find_if_in_interval(light, light_to_temperature, temperature_to_humidity, jump)
+    hum = find_if_in_interval(temp, temperature_to_humidity, humidity_to_location, jump)
+    loc = find_if_in_interval(hum, humidity_to_location, humidity_to_location, jump)
 
     if loc < lowest_location:
       lowest_location = loc
-    index += 1
+    if min(jump) > 0:
+      print("Jump:", min(jump))
+    index += min(jump)
+    if (index == key+value):
+      index += 1
   print()
 
 print(lowest_location)
 
-print("Process finished --- %s seconds ---" % (time.time() - start_time))
+print("Process finished --- %s seconds ---" % round(time.time() - start_time, 4))
