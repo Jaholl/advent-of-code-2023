4a9a7c1b067e47982e0af1f54194d84b3a9434a6
Joel Bångdal <j.bangdal@gmail.com>
2023-12-07 08:33:06 +0100

    Can't do more now

--- dayx/solvep2.py
+++ dayx/solvep2.py
@@ -1,6 +1,6 @@
 import re, time
 
 start_time = time.time()
-file = open("dayx/input.txt", "r").readlines()
+file = open("day7/input.txt", "r").readlines()
 
 print("Process finished --- %s seconds ---" % round(time.time() - start_time, 4))
